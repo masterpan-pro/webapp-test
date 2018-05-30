67e908744fe54bdb5d21e9ca751f0ae05ee22b1e
MasterPan <mwolfer@foxmail.com>
2018-05-30 10:11:47 +0800

    update

--- src/main/test/com/test/TestDemo.java
+++ src/main/test/com/test/TestDemo.java
@@ -1,15 +1,11 @@
 package com.test;
 
-import com.demo.util.DateUtils;
-
 /**
  * Tests
  */
 public class TestDemo {
     public static void main(String[] args) {
 
-        String formatDate = DateUtils.formatTransfrom("2018-05-30", DateUtils.FULL_DATE_PATTERN, "yyyyMMdd");
-        System.out.println(formatDate);
 
     }
 }
