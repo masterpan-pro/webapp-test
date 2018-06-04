7885bca29ed53023a4358db867ce27489e505743
MasterPan <mwolfer@foxmail.com>
2018-06-04 10:14:04 +0800

    update

--- src/test/java/com/test/TestDemo.java
+++ src/test/java/com/test/TestDemo.java
@@ -6,12 +6,12 @@ package com.test;
 public class TestDemo {
     public static void main(String[] args) {
         for (int i = 16; i <= 30; i++) {
-            System.out.println("INSERT INTO `t_trans_file` VALUES (null, 'DJJD201805" + i + ".xls', null, null, '2018-05-" + i + "', null, 'DJJD', false);");
-            System.out.println("INSERT INTO `t_trans_file` VALUES (null, 'HB201805" + i + ".xls', null, null, '2018-05-" + i + "', null, 'HB', false);");
-            System.out.println("INSERT INTO `t_trans_file` VALUES (null, 'JZMX201805" + i + ".txt', null, null, '2018-05-" + i + "', null, 'JZMX', false);");
-            System.out.println("INSERT INTO `t_trans_file` VALUES (null, 'WTCZ201805" + i + ".xls', null, null, '2018-05-" + i + "', null, 'WTCZ', false);");
-            System.out.println("INSERT INTO `t_trans_file` VALUES (null, 'WTTX201805" + i + ".xls', null, null, '2018-05-" + i + "', null, 'WTTX', false);");
-            System.out.println("INSERT INTO `t_trans_file` VALUES (null, 'YE201805" + i + ".xls', null, null, '2018-05-" + i + "', null, 'YE', false);");
+            System.out.println("INSERT INTO `t_trans_file` VALUES (null, 'DJJD201806" + i + ".xls', null, null, '2018-06-" + i + "', null, 'DJJD', false);");
+            System.out.println("INSERT INTO `t_trans_file` VALUES (null, 'HB201806" + i + ".xls', null, null, '2018-06-" + i + "', null, 'HB', false);");
+            System.out.println("INSERT INTO `t_trans_file` VALUES (null, 'JZMX201806" + i + ".txt', null, null, '2018-06-" + i + "', null, 'JZMX', false);");
+            System.out.println("INSERT INTO `t_trans_file` VALUES (null, 'WTCZ201806" + i + ".xls', null, null, '2018-06-" + i + "', null, 'WTCZ', false);");
+            System.out.println("INSERT INTO `t_trans_file` VALUES (null, 'WTTX201806" + i + ".xls', null, null, '2018-06-" + i + "', null, 'WTTX', false);");
+            System.out.println("INSERT INTO `t_trans_file` VALUES (null, 'YE201806" + (i - 1) + ".xls', null, null, '2018-06-" + i + "', null, 'YE', false);");
         }
     }
 }
