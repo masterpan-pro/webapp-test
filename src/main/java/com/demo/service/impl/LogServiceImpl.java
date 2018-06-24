3d730bfe01586800158c046414d62b6c5385d923
pengpan <mwolfer@foxmail.com>
2018-06-25 00:10:27 +0800

    update

--- src/main/java/com/demo/service/impl/LogServiceImpl.java
+++ src/main/java/com/demo/service/impl/LogServiceImpl.java
@@ -3,16 +3,22 @@ package com.demo.service.impl;
 import com.demo.dao.LogMapper;
 import com.demo.entity.Log;
 import com.demo.service.LogService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+@Slf4j
 @Service
 public class LogServiceImpl implements LogService {
 
     @Resource
     private LogMapper logMapper;
+
+    @Resource
+    private JdbcTemplate jdbcTemplate;
 
     @Override
     public int insert(Log log) {
@@ -36,6 +42,8 @@ public class LogServiceImpl implements LogService {
 
     @Override
     public List<Log> find() {
+        List<Long> count = jdbcTemplate.queryForList("select count(*) from log", Long.class);
+        log.info("Log Count: {}", count);
         return logMapper.find();
     }
 }
