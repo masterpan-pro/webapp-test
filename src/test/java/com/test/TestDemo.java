f72ace3228c16791d9aef246ac7f1b4aa73f6eed
Micro <micro.pan@outlook.com>
2018-07-17 12:34:14 +0800

    update

--- src/test/java/com/test/TestDemo.java
+++ src/test/java/com/test/TestDemo.java
@@ -2,10 +2,12 @@ package com.test;
 
 import com.test.base.BaseJunitTest;
 import org.junit.Test;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * Tests
  */
+@WebAppConfiguration
 public class TestDemo extends BaseJunitTest {
 
     @Test
