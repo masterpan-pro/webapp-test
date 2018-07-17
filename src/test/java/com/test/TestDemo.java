036e39114361f1e8c599f8f384bc469092e5c133
Micro <micro.pan@outlook.com>
2018-07-17 12:49:17 +0800

    update

--- src/test/java/com/test/TestDemo.java
+++ src/test/java/com/test/TestDemo.java
@@ -1,7 +1,11 @@
 package com.test;
 
+import com.demo.dao.UserMapper;
 import com.test.base.BaseJunitTest;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
@@ -10,8 +14,15 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 public class TestDemo extends BaseJunitTest {
 
-    @Test
-    public void test(){
+    @Autowired
+    SqlSessionFactory sqlSessionFactory;
 
+    @Test
+    public void test() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.find().forEach(System.out::println);
+        System.out.println("----------[Mybatis一级缓存]----------");
+        mapper.find().forEach(System.out::println);
     }
 }
